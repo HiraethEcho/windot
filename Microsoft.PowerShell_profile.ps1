2c3bae238538b8cf577fece1a198d0e44cd3f9fb
wyzwin <wyz2016zxc@outlook.com>
2023-07-10 10:29:10 +0800

    init, add lfrc and powershell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module posh-git
+
+Import-Module -Name Terminal-Icons
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineOption -PredictionViewStyle ListView
+function lg {lazygit}
+function ee {explorer .}
+function glog {git log --graph --oneline --decorate}
+function s {fastfetch}
+function hd {hexo clean ; hexo g; hexo d; hexo clean}
