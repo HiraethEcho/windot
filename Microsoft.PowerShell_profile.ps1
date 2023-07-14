69b2ce4dd90938281bee26b40194a63d522f5f20
wyzwin <wyz2016zxc@outlook.com>
2023-07-14 19:25:44 +0800

    add NVIM_APPNAME

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,3 +8,7 @@ function ee {explorer .}
 function glog {git log --graph --oneline --decorate}
 function s {fastfetch}
 function hd {hexo clean ; hexo g; hexo d; hexo clean}
+function hs {hexo clean ; hexo g; hexo s -o}
+function lazyvim {$ENV:NVIM_APPNAME = "lazyvim" ;; nvim ;; $ENV:NVIM_APPNAME=''}
+function nvchard {$ENV:NVIM_APPNAME = "nvchard" ;; nvim ;; $ENV:NVIM_APPNAME=''}
+function astronvim {$ENV:NVIM_APPNAME = "astronvim" ;; nvim ;; $ENV:NVIM_APPNAME=''}
