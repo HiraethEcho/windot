2ba7c5278d3cfacd6fd83f6f243adafcc61b90bd
wyzwin <wyz2016zxc@outlook.com>
2023-07-14 22:59:59 +0800

    merge 2 ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,1 @@
-﻿Import-Module posh-git
-
-Import-Module -Name Terminal-Icons
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineOption -PredictionViewStyle ListView
-function lg {lazygit}
-function ee {explorer .}
-function glog {git log --graph --oneline --decorate}
-function s {fastfetch}
-function hd {hexo clean ; hexo g; hexo d; hexo clean}
-function hs {hexo clean ; hexo g; hexo s -o}
-function lazyvim {$ENV:NVIM_APPNAME = "lazyvim" ;; nvim ;; $ENV:NVIM_APPNAME=''}
-function nvchard {$ENV:NVIM_APPNAME = "nvchard" ;; nvim ;; $ENV:NVIM_APPNAME=''}
-function astronvim {$ENV:NVIM_APPNAME = "astronvim" ;; nvim ;; $ENV:NVIM_APPNAME=''}
+D:/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
