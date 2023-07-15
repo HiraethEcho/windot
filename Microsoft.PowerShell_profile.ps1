9bc97f7dc69820af9e074c610b4ac490c12be4e7
wyzwin <wyz2016zxc@outlook.com>
2023-07-16 00:52:22 +0800

    mklink does not work

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,32 @@
-D:/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+﻿Import-Module -Name Terminal-Icons
+
+Import-Module posh-git
+
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+function ee {explorer .}
+function l {ls}
+
+function lg {lazygit}
+function glog {git log --graph --oneline --decorate}
+function gh {git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --all}
+
+function c. {start nvim $PROFILE}
+
+function s {fastfetch}
+function starwar {telnet towel.blinkenlights.nl}
+
+
+function hd {hexo clean ; hexo g; hexo d; hexo clean}
+function hs {hexo clean ; hexo g; hexo s -o}
+
+function lazyvim {$ENV:NVIM_APPNAME = "lazyvim" ;; nvim ;; $ENV:NVIM_APPNAME=''}
+function nvchard {$ENV:NVIM_APPNAME = "nvchard" ;; nvim ;; $ENV:NVIM_APPNAME=''}
+function astronvim {$ENV:NVIM_APPNAME = "astronvim" ;; nvim ;; $ENV:NVIM_APPNAME=''}
